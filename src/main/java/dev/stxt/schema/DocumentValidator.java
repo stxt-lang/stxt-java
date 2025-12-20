7926baad66867ed06c3f284f944eb2ee9be4e7d3
Joan Costa Mombiela <joan.costa.mombiela@gmail.com>
2025-12-20 21:41:45 +0100

    M+

--- src/main/java/dev/stxt/schema/DocumentValidator.java
+++ src/main/java/dev/stxt/schema/DocumentValidator.java
@@ -1,12 +1,15 @@
 package dev.stxt.schema;
 
 import dev.stxt.IOException;
+
+import java.util.Collections;
 import java.util.List;
 
 import dev.stxt.Node;
 import dev.stxt.ParseException;
 import dev.stxt.resources.NotFoundException;
 import dev.stxt.resources.ResourcesLoader;
+import dev.stxt.validator.TopDownValidator;
 
 public final class DocumentValidator {
 	private final SchemaValidator validator;
@@ -16,10 +19,13 @@ public final class DocumentValidator {
 	}
 
 	public void validateNode(Node node) throws ParseException, IOException, NotFoundException {
-		validator.validateNode(node);
+		TopDownValidator tdp = new TopDownValidator(Collections.singletonList(validator));
+		tdp.validate(node);
 	}
 
 	public void validateNodes(List<Node> nodes) throws ParseException, IOException, NotFoundException {
-		validator.validateNodes(nodes);
+		TopDownValidator tdp = new TopDownValidator(Collections.singletonList(validator));
+		for (Node n: nodes)
+			tdp.validate(n);
 	}
 }
