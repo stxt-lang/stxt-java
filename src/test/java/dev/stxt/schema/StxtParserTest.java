6c9d66d5fb749cfeadfff4557179e3879ce32c61
Joan Costa Mombiela <joan.costa.mombiela@gmail.com>
2025-12-20 19:44:43 +0100

    M+

---
+++ src/test/java/dev/stxt/schema/StxtParserTest.java
@@ -0,0 +1,41 @@
+package dev.stxt.schema;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+import dev.stxt.Node;
+import dev.stxt.ParseException;
+import dev.stxt.Parser;
+import dev.stxt.resources.ResourcesException;
+import dev.stxt.resources.ResourcesLoader;
+import dev.stxt.resources.ResourcesLoaderDirectory;
+import test.FileTestLoction;
+import test.JSON;
+
+public class StxtParserTest {
+	@Test
+	void testParser() throws ResourcesException, IOException, ParseException {
+		System.out.println("Inici");
+		
+		Parser parser = new Parser();
+		
+		ResourcesLoader resourcesLoader = new ResourcesLoaderDirectory(FileTestLoction.getFile(""));
+		SchemaProviderCache schemaLocator = new SchemaProviderCache(resourcesLoader);
+		SchemaValidator schemaValidator = new SchemaValidator(schemaLocator);
+		
+		parser.registerValidator(schemaValidator);
+
+		File f = FileTestLoction.getFile("docs/client.stxt");
+		System.out.println("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+		System.out.println("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+		System.out.println("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+		System.out.println(f.getAbsolutePath());
+		List<Node> docs = parser.parseFile(f);
+		for (Node doc: docs)
+			System.out.println(JSON.toJsonPretty(doc));
+	}	
+	
+}
