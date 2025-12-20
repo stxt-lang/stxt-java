1ba176667a3451105ce0786d07db69c2cd8d57d4
Joan Costa Mombiela <joan.costa.mombiela@gmail.com>
2025-12-20 21:52:15 +0100

    M+

--- src/test/java/dev/stxt/schema/SchemaValidatorTest.java
+++ src/test/java/dev/stxt/schema/SchemaValidatorTest.java
@@ -18,21 +18,25 @@ import test.JSON;
 public class SchemaValidatorTest {
 	@Test
 	void testReadSchema() throws IOException, ParseException, ResourcesException {
-		// Path
-		ResourcesLoader resourcesLoader = new ResourcesLoaderDirectory(FileTestLoction.getFile(""));
-
-		// Validator
-		DocumentValidator schemaValidator = new DocumentValidator(resourcesLoader);
-
 		// Parser file
 		File f = FileTestLoction.getFile("docs/email.stxt");
-		Parser parser = new Parser();
+		Parser parser = getParser();
 		List<Node> nodes = parser.parseFile(f);
-		schemaValidator.validateNodes(nodes);
 
 		for (Node n : nodes) {
 			System.out.println(JSON.toJsonPretty(n));
 		}
 	}
 
+	private Parser getParser() throws ResourcesException
+	{
+		Parser result = new Parser();
+		// Path
+		ResourcesLoader resourcesLoader = new ResourcesLoaderDirectory(FileTestLoction.getFile(""));
+		SchemaValidator schemaValidator = new SchemaValidator(new SchemaProviderCache(resourcesLoader));
+
+		result.registerValidator(schemaValidator);
+		
+		return result;
+	}
 }
