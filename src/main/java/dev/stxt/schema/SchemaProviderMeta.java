1772f812d92dcbc9333020025819f929d6a26fc7
Joan Costa Mombiela <joan.costa.mombiela@gmail.com>
2025-12-23 18:37:20 +0100

    M+

--- src/main/java/dev/stxt/schema/SchemaProviderMeta.java
+++ src/main/java/dev/stxt/schema/SchemaProviderMeta.java
@@ -5,16 +5,27 @@ import java.util.List;
 import dev.stxt.Node;
 import dev.stxt.Parser;
 import dev.stxt.resources.ResourceNotFoundException;
-import dev.stxt.utils.ResourceReader;
 
 final class SchemaProviderMeta implements SchemaProvider {
+	private static final String META_TEXT = "Schema (@stxt.schema): @stxt.schema\n"
+			+ "    Node: Schema (INLINE TEXT)\n"
+			+ "        Children>>\n"
+			+ "            Description (?)\n"
+			+ "            Node (+)\n"
+			+ "    Node: Node (INLINE TEXT)\n"
+			+ "        Children>>\n"
+			+ "            Children (?)\n"
+			+ "            Description (?)\n"
+			+ "    Node: Children (MULTILINE TEXT)\n"
+			+ "    Node: Description (TEXT)\n"
+			+ "";
+	
 	private final Schema meta;
 
 	public SchemaProviderMeta() {
 		Schema metaSchema = null;
-		String metaSchemaText = ResourceReader.readResource("dev/stxt/schema/@stxt.schema.stxt");
 		Parser parser = new Parser();
-		List<Node> nodes = parser.parse(metaSchemaText);
+		List<Node> nodes = parser.parse(META_TEXT);
 		metaSchema = SchemaParser.transformNodeToSchema(nodes.get(0));
 		meta = metaSchema;
 	}
