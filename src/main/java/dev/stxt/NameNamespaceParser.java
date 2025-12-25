21dc388b179a9c1217176752f27ba7f7a27ace65
Joan Costa Mombiela <joan.costa.mombiela@gmail.com>
2025-12-25 11:11:59 +0100

    M+

--- src/main/java/dev/stxt/NameNamespaceParser.java
+++ src/main/java/dev/stxt/NameNamespaceParser.java
@@ -52,3 +52,22 @@ public final class NameNamespaceParser {
         return new NameNamespace(name, namespace);
     }
 }
+
+final class NameNamespace {
+    private final String name;
+    private final String namespace;
+
+    public NameNamespace(String name, String namespace) {
+        this.name = name;
+        this.namespace = namespace;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getNamespace() {
+        return namespace;
+    }
+}
+
