9d6b46dd5aa3924cc9f89fab286daaf6d5792a2f
Joan Costa Mombiela <joan.costa.mombiela@gmail.com>
2025-12-28 11:30:59 +0100

    M+

--- src/main/java/dev/stxt/schema/TemplateSchemaParser.java
+++ src/main/java/dev/stxt/schema/TemplateSchemaParser.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import dev.stxt.Node;
 import dev.stxt.ParseException;
+import dev.stxt.utils.StringUtils;
 
 public class TemplateSchemaParser {
 
@@ -35,45 +36,42 @@ public class TemplateSchemaParser {
 		
 		// Miramos si es nuevo y añadimos en listado
 		SchemaNode schemaNode = schema.getNodes().get(name);
-		String type = cl.getType() == null? "INLINE TEXT": cl.getType();
 		if (schemaNode == null) {	// Nuevo
+			String type = cl.getType() == null? "INLINE TEXT": cl.getType();
 			schemaNode = new SchemaNode();
 			schemaNode.setName(name);
 			schemaNode.setType(type);
 			schema.getNodes().put(name, schemaNode);
 		} else {
-			if (cl.getType()!=null)
-				throw new ParseException(node.getLine(), "TYPE_DEFINED_MULTIPLE_TIMES", "Type only can defined one time");
+			String type = StringUtils.normalizeSimple(cl.getType());
+			if (type.equals("@" + name)) return; // OK Definition
+			if (type.startsWith("@"))
+				new ParseException(node.getLine(), "NODE_REFERENCE_NOT_VALID", "Reference must be '" + "@" + name + "', not '" + type + "'");
+			throw new ParseException(node.getLine(), "NODE_DEFINED_MULTIPLE_TIMES", "Nodes only can defined the first time: " + name);
 		}
 		
 		// Una vez ya existe, si tiene hijos los intentamos crear.
 		List<Node> childrenNode = node.getChildren();
-		if (childrenNode.size()>0) {
-			// Si ya tenía error!!
-			if(schemaNode.getChildren().size()>0)
-				throw new ParseException(node.getLine(), "CHILDREN_DEFINED_MULTIPLE_TIMES", "children only can defined one time");
+		
+		// Insertamos childs
+		for (Node child: childrenNode) {
+			System.out.println("=>" + child.getName() + " -> " + child.getNamespace());
+			cl = TemplateChildLineParser.parse(child.getInlineText(), child.getLine());
+			System.out.println(" ==> " + cl);
 			
-			// Insertamos childs
-			for (Node child: childrenNode) {
-				System.out.println("=>" + child.getName() + " -> " + child.getNamespace());
-				cl = TemplateChildLineParser.parse(child.getInlineText(), child.getLine());
-				System.out.println(" ==> " + cl);
-				
-				String childName = child.getName();
-				String childNamespace = child.getNamespace();
-				if (childNamespace.equals("@stxt.template")) childNamespace = schema.getNamespace();
-				String childQualifiedName = childNamespace + ":" + childName;
-				
-				SchemaChild schChild = new SchemaChild();
-				schChild.setName(childName);
-				schChild.setNamespace(childNamespace);
-				schChild.setMin(cl.getMin());
-				schChild.setMax(cl.getMax());
-				schemaNode.getChildren().put(childQualifiedName, schChild);
-				
-				addToSchema(schema, child);
-			}
+			String childName = child.getName();
+			String childNamespace = child.getNamespace();
+			if (childNamespace.equals("@stxt.template")) childNamespace = schema.getNamespace();
+			String childQualifiedName = childNamespace + ":" + childName;
+			
+			SchemaChild schChild = new SchemaChild();
+			schChild.setName(childName);
+			schChild.setNamespace(childNamespace);
+			schChild.setMin(cl.getMin());
+			schChild.setMax(cl.getMax());
+			schemaNode.getChildren().put(childQualifiedName, schChild);
+			
+			addToSchema(schema, child);
 		}
 	}
-
 }
