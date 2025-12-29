29ab6aff1994012eeeda2818bd6a1426041a8d71
Joan Costa Mombiela <joan.costa.mombiela@gmail.com>
2025-12-29 14:13:52 +0100

    M+

--- src/test/java/dev/stxt/utils/NormalizeFullTest.java
+++ src/test/java/dev/stxt/utils/NormalizeFullTest.java
@@ -3,15 +3,19 @@ package dev.stxt.utils;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-public class StringUtilsTest {
+public class NormalizeFullTest {
 	@Test
 	void demo() {
 		checkEquals("Hola Mundo", "hola-mundo");
 		checkEquals("   Hola   Mundo ", "hola-mundo");
 		checkEquals("   Hola${}:   Mundo ", "hola-mundo");
+		checkEquals("   Hola_ Mundo ", "hola-mundo");
+		checkEquals("   Hola_Mundo ", "hola-mundo");
+		checkEquals("   Hólä_Mundo ", "hola-mundo");
 	}
 
 	private void checkEquals(String normal, String normalized) {
+		System.out.println(normal + " => " + StringUtils.normalizeFull(normal));
 		Assertions.assertEquals(StringUtils.normalizeFull(normal), normalized);
 		
 	}
