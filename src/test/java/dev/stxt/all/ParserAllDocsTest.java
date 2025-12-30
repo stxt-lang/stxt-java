da85f68bb313523686e3ea12f83df5b3edd261c6
Joan Costa Mombiela <joan.costa.mombiela@gmail.com>
2025-12-30 17:07:03 +0100

    M+

---
+++ src/test/java/dev/stxt/all/ParserAllDocsTest.java
@@ -0,0 +1,48 @@
+package dev.stxt.all;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+import dev.stxt.Node;
+import dev.stxt.Parser;
+import dev.stxt.STXTParser;
+import dev.stxt.exceptions.ParseException;
+import dev.stxt.resources.ResourcesLoaderDirectory;
+import dev.stxt.utils.FileUtils;
+import test.FileTestLoction;
+import test.JSONFile;
+
+public class ParserAllDocsTest {
+	public static void main(String[] args) throws IOException, ParseException {
+		new ParserAllDocsTest().mainTest();
+	}
+
+	@Test
+	public void mainTest() throws IOException, ParseException {
+		System.out.println("Inici");
+
+		// Create parser
+		Parser parser = new STXTParser(new ResourcesLoaderDirectory(FileTestLoction.getFile("")));
+		File docsDir = FileTestLoction.getFile("docs");
+
+		List<File> stxtFiles = FileUtils.getStxtFiles(docsDir);
+
+		for (File file : stxtFiles) {
+			validateFile(parser, file);
+		}
+
+		System.out.println("End");
+	}
+
+	private void validateFile(Parser parser, File file) throws IOException, ParseException {
+		System.out.println("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+		System.out.println(file.getAbsolutePath());
+		List<Node> docs = parser.parseFile(file);
+		for (Node node : docs) {
+			JSONFile.checkContentWithJsonFile(node, "docs_json/", file.getName().substring(0, file.getName().length() - 5));
+		}
+	}
+}
---
+++ src/test/java/dev/stxt/all/ParserAllErrorDocsTest.java
@@ -0,0 +1,54 @@
+package dev.stxt.all;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.junit.jupiter.api.Test;
+
+import dev.stxt.Parser;
+import dev.stxt.STXTParser;
+import dev.stxt.exceptions.ParseException;
+import dev.stxt.resources.ResourcesLoaderDirectory;
+import dev.stxt.utils.FileUtils;
+import test.FileTestLoction;
+import test.JSONFile;
+
+public class ParserAllErrorDocsTest {
+	public static void main(String[] args) throws IOException {
+		new ParserAllErrorDocsTest().mainTest();
+	}
+
+	@Test
+	public void mainTest() throws IOException {
+		System.out.println("Inici");
+
+		// Create parser
+		Parser parser = new STXTParser(new ResourcesLoaderDirectory(FileTestLoction.getFile("")));
+		File docsDir = FileTestLoction.getFile("error_schema");
+
+		List<File> stxtFiles = FileUtils.getStxtFiles(docsDir);
+
+		for (File file : stxtFiles) {
+			try {
+				System.out.println("***************************************************");
+				System.out.println("FILE: " + file.getAbsolutePath());
+				parser.parseFile(file);
+				fail("Debería haber saltado excepción!!");
+			} catch (ParseException e) {
+				Map<String, Object> errorInfo = new HashMap<>();
+				errorInfo.put("line", e.getLine());
+				errorInfo.put("code", e.getCode());
+				System.out.println("Error: " + e.getMessage());
+
+				JSONFile.checkContentWithJsonFile(errorInfo, "error_schema_json", file.getName().substring(0, file.getName().length() - 5));
+			}
+		}
+
+		System.out.println("End");
+	}
+}
