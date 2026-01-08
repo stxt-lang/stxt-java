74d217d3efb0d38e5937efbaf0b0e3764482ed26
Joan Costa <jcostamo@fiatc.es>
2026-01-08 07:49:53 +0100

    M+

--- src/main/java/dev/stxt/LineIndentParser.java
+++ src/main/java/dev/stxt/LineIndentParser.java
@@ -15,14 +15,8 @@ class LineIndentParser {
     
 	public static LineIndent parseLine(String line, int numLine, ParseState parseState) {
 		int stackSize = parseState.getStack().size();
-		boolean lastNodeText = stackSize > 0 && parseState.getStack().peek().isTextNode();
-
-		if (lastNodeText) return parseLineAfterBlock(stackSize, line, numLine);
-		else              return parseLineAfterInline(stackSize, line, numLine);
-	}
-
-	private static LineIndent parseLineAfterBlock(int stackSize, String line, int numLine)
-    {
+		boolean lastNodeBlock = stackSize > 0 && parseState.getStack().peek().isTextNode();
+		
         // Recorremos
         int level = 0;
         int spaces = 0;
@@ -43,65 +37,30 @@ class LineIndentParser {
             } else if (c == COMMENT_CHAR) {
                 return null;
             } else {
-                // Primer carácter no espacio/tab => fin de indentación
+                // Primer carácter no espacio/tab/comentario => fin de indentación
                 break;
             }
 
             pointer++;
 
-            // Dentro de multilínea: no consumir más niveles de los del nodo
-            if (level >= stackSize) 
+            // Dentro del bloque de texto
+            if (lastNodeBlock && level >= stackSize) 
                 return new LineIndent(level, rightTrim(line.substring(pointer)));
-        }
+        }        
+        
+        // En este punto ya estamos fuera de bloque de texto (si existía)
         
         // Empty
-        if (pointer == line.length()) return new LineIndent(stackSize, "");
-
-        // Strange indentation
-        if (spaces > 0)
-            throw new ParseException(numLine, "INVALID_NUMBER_SPACES", "There are " + spaces + " spaces before node");
-
-        // 4) Caso general: devolver la línea sin la indentación consumida
-        return new LineIndent(level, line.substring(pointer).trim());
-    }
-
-    private static LineIndent parseLineAfterInline(int stackSize, String line, int numLine)
-    {
-        // Recorremos
-        int level = 0;
-        int spaces = 0;
-        int pointer = 0;
-
-        while (pointer < line.length()) {
-            char c = line.charAt(pointer);
-
-            if (c == SPACE) {
-                spaces++;
-                if (spaces == TAB_SPACES) {
-                    level++;
-                    spaces = 0;
-                }
-            } else if (c == TAB) {
-                level++;
-                spaces = 0;
-            } else if (c == COMMENT_CHAR) {
-                return null;
-            } else {
-                // Primer carácter no espacio/tab → fin de indentación
-                break;
-            }
-
-            pointer++;
+        if (pointer == line.length()) {
+            if (lastNodeBlock)  return new LineIndent(stackSize, "");
+            else                return null;
         }
 
-        // Empty
-        if (pointer == line.length()) return null;
-
-        // Strange indentation
+        // Indentación no és múltiplo de 4 con espacios
         if (spaces > 0)
             throw new ParseException(numLine, "INVALID_NUMBER_SPACES", "There are " + spaces + " spaces before node");
 
         // 4) Caso general: devolver la línea sin la indentación consumida
         return new LineIndent(level, line.substring(pointer).trim());
-    }
+	}
 }
