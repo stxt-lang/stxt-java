2904b9ceb033e34642d6f6a5736267300d7ec60e
Joan Costa Mombiela <joan.costa.mombiela@gmail.com>
2026-01-09 23:13:33 +0100

    M+

--- src/main/java/dev/stxt/exceptions/ParseException.java
+++ src/main/java/dev/stxt/exceptions/ParseException.java
@@ -1,9 +1,13 @@
 package dev.stxt.exceptions;
 
-public class ParseException extends LineAwareException {
+public class ParseException extends STXTException {
 	private static final long serialVersionUID = 1L;
 	
+	private final int line;
+    
     public ParseException(int line, String code, String message) {
-        super(line, code, message);
+        super(code, "Error at line: " + line + ", " + message);
+        this.line = line;
     }
+    public int getLine() { return line; }
 }
